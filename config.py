a15e79a1c2c8cd41abb6f94f7b5182fc56e96686
Yves Z <zendjayves@yahoo.fr>
2021-01-04 22:24:20 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 config_vars = {
-    'project_id': 'covid-297706',
-    'output_dataset_id': 'covid_data',
-    'output_table_name': 'malades',
-    'sql_file_path': 'covid_query.sql'
+    'project_id': 'exampletrigger',
+    'output_dataset_id': 'exampleDataset',
+    'output_table_name': 'exampleData'
 }
